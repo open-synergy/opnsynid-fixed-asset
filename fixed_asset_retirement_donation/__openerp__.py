644397ee0390df5b6c63b1cd16ca4e39162eac29
mikevhe18 <viriyananda.michael@gmail.com>
2018-11-16 16:27:07 +0700

    [8.0.1.0.0][fixed_asset_retirement_donation]

---
+++ fixed_asset_retirement_donation/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Fixed Asset Retirement by Donation",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "fixed_asset_retirement_common",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "data/account_asset_retirement_type_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_workflow_policy_data.xml",
+        "views/account_asset_retirement_donation_views.xml",
+    ]
+}
