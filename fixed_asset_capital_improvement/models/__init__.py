ca6385cc57454f9e2a85fceac55e0b24f901d1c3
Andhitia Rama <andhitia.r@gmail.com>
2018-11-22 23:10:17 +0800

    [8.0.1.0.0][fixed_asset_capital_improvement]

---
+++ fixed_asset_capital_improvement/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    account_asset_asset,
+    account_asset_category,
+    account_asset_improvement,
+    res_company,
+    res_config,
+    tier_definition,
+)
---
+++ fixed_asset_capital_improvement/models/res_company.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    asset_improvement_sequence_id = fields.Many2one(
+        string="Fixed Asset Improvement Sequence",
+        comodel_name="ir.sequence",
+    )
+    asset_improvement_confirm_grp_ids = fields.Many2many(
+        string="Allowed To Confirm Fixed Asset Improvement",
+        comodel_name="res.groups",
+        relation="rel_company_asset_improvement_allowed_confirm",
+        column1="company_id",
+        column2="group_id",
+    )
+    asset_improvement_valid_grp_ids = fields.Many2many(
+        string="Allowed To Validate Fixed Asset Improvement",
+        comodel_name="res.groups",
+        relation="rel_company_asset_improvement_allowed_valid",
+        column1="company_id",
+        column2="group_id",
+    )
+    asset_improvement_cancel_grp_ids = fields.Many2many(
+        string="Allowed To Cancel Fixed Asset Improvement",
+        comodel_name="res.groups",
+        relation="rel_company_asset_improvement_allowed_cancel",
+        column1="company_id",
+        column2="group_id",
+    )
+    asset_improvement_restart_grp_ids = fields.Many2many(
+        string="Allowed To Restart Fixed Asset Improvement",
+        comodel_name="res.groups",
+        relation="rel_company_asset_improvement_allowed_restart",
+        column1="company_id",
+        column2="group_id",
+    )
+    asset_improvement_restart_validation_grp_ids = fields.Many2many(
+        string="Allowed To Restart Validation Fixed Asset Improvement",
+        comodel_name="res.groups",
+        relation="rel_company_asset_improvement_allowed_restart_validation",
+        column1="company_id",
+        column2="group_id",
+    )
