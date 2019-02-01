78dafea336c6d6c5edfd779333989912a864bd91
Andhitia Rama <andhitia.r@gmail.com>
2019-02-01 11:32:22 +0700

    account_asset_management_qrcode

---
+++ account_asset_management_qrcode/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Fixed Asset QR Code",
+    "version": "8.0.1.1.2",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_asset_management",
+        "base_qr_code",
+    ],
+    "data": [
+        "views/account_asset_views.xml",
+    ]
+}
