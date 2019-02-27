3b1cc65b3e65ebbbf910f5a45f130fbbd3db4df5
Andhitia Rama <andhitia.r@gmail.com>
2019-02-27 11:05:09 +0700

    account_asset_management_estimation_change]
    * Adjusting change on account_asset_management_extend ver 8.0.2.0.0

--- account_asset_management_estimation_change/__openerp__.py
+++ account_asset_management_estimation_change/__openerp__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fixed Asset Change of Estimation",
-    "version": "8.0.1.1.1",
+    "version": "8.0.2.0.0",
     "category": "Accounting & Finance",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "account_asset_management_extend",
+        "account_asset_management_depreciation_line_subtype",
         "account_asset_management_config_page",
         "base_sequence_configurator",
         "base_workflow_policy",
@@ -22,6 +22,7 @@
         "data/ir_sequence_data.xml",
         "data/base_sequence_configurator_data.xml",
         "data/base_workflow_policy_data.xml",
+        "data/account_asset_depreciation_line_subtype_data.xml",
         "menu.xml",
         "views/account_asset_asset_views.xml",
         "views/account_asset_config_setting_views.xml",
