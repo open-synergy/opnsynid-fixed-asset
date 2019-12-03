4637629fa8c9ec4dccf90b5fd3759d9f80694702
Andhitia Rama <andhitia.r@gmail.com>
2019-12-03 16:02:16 +0700

    account_asset_management_qrcode
    * Adjust to newest qr module

--- account_asset_management_qrcode/__openerp__.py
+++ account_asset_management_qrcode/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fixed Asset QR Code",
-    "version": "8.0.1.1.2",
+    "version": "8.0.1.2.0",
     "category": "Accounting & Finance",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -15,6 +15,7 @@
         "base_qr_code",
     ],
     "data": [
+        "data/base_qr_content_policy_data.xml",
         "views/account_asset_views.xml",
     ]
 }
