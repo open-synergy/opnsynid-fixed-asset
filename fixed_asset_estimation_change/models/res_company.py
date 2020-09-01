f59902efd825ab7acff5f985aece7be10dfe0d91
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-01 19:53:19 +0700

    [8.0.1.0.0][fixed_asset_estimation_change] Fixing Travis Error

--- fixed_asset_estimation_change/models/res_company.py
+++ fixed_asset_estimation_change/models/res_company.py
@@ -3,7 +3,7 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class ResCompany(models.Model):
@@ -100,61 +100,3 @@ class ResCompany(models.Model):
         column1="company_id",
         column2="group_id",
     )
-
-    # @api.model
-    # def _get_asset_useful_life_button_policy_map(self):
-    #     return [
-    #         ("confirm_ok",
-    #             "asset_useful_life_confirm_grp_ids"),
-    #         ("valid_ok", "asset_useful_life_valid_grp_ids"),
-    #         ("open_ok", "asset_useful_life_open_grp_ids"),
-    #         ("cancel_ok", "asset_useful_life_cancel_grp_ids"),
-    #         ("restart_ok", "asset_useful_life_restart_grp_ids"),
-    #     ]
-    #
-    # @api.multi
-    # def _get_asset_useful_life_button_policy(self, policy_field):
-    #     self.ensure_one()
-    #     result = False
-    #     button_group_ids = []
-    #     user = self.env.user
-    #     group_ids = user.groups_id.ids
-    #
-    #     button_group_ids += getattr(
-    #         self, policy_field).ids
-    #
-    #     if not button_group_ids:
-    #         result = True
-    #     else:
-    #         if (set(button_group_ids) & set(group_ids)):
-    #             result = True
-    #     return result
-    #
-    # @api.model
-    # def _get_asset_salvage_button_policy_map(self):
-    #     return [
-    #         ("confirm_ok",
-    #             "asset_salvage_confirm_grp_ids"),
-    #         ("valid_ok", "asset_salvage_valid_grp_ids"),
-    #         ("open_ok", "asset_salvage_open_grp_ids"),
-    #         ("cancel_ok", "asset_salvage_cancel_grp_ids"),
-    #         ("restart_ok", "asset_salvage_restart_grp_ids"),
-    #     ]
-    #
-    # @api.multi
-    # def _get_asset_salvage_button_policy(self, policy_field):
-    #     self.ensure_one()
-    #     result = False
-    #     button_group_ids = []
-    #     user = self.env.user
-    #     group_ids = user.groups_id.ids
-    #
-    #     button_group_ids += getattr(
-    #         self, policy_field).ids
-    #
-    #     if not button_group_ids:
-    #         result = True
-    #     else:
-    #         if (set(button_group_ids) & set(group_ids)):
-    #             result = True
-    #     return result
