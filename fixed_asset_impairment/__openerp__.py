b5eae7529cedf4a14dc45e1d71a31bf3fa589f2d
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-08 11:07:34 +0700

    [8.0.1.0.0][fixed_asset_impairment]

--- fixed_asset_impairment/__openerp__.py
+++ fixed_asset_impairment/__openerp__.py
@@ -4,8 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
-    "name": "Fixed Asset Change of Estimation",
-    "version": "8.0.1.0.1",
+    "name": "Fixed Asset Impairment",
+    "version": "8.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -15,21 +15,21 @@
         "fixed_asset",
         "base_sequence_configurator",
         "base_workflow_policy",
-        "web_readonly_bypass",
         "base_print_policy",
         "base_multiple_approval",
     ],
     "data": [
-        "security/res_groups_data.xml",
         "security/ir.model.access.csv",
+        "security/res_groups_data.xml",
         "data/ir_sequence_data.xml",
         "data/base_sequence_configurator_data.xml",
         "data/base_workflow_policy_data.xml",
-        "data/account_asset_depreciation_line_subtype_data.xml",
         "menu.xml",
-        "views/account_asset_asset_views.xml",
         "views/account_asset_config_setting_views.xml",
-        "views/account_asset_change_estimation_useful_life_views.xml",
-        "views/account_asset_change_estimation_salvage_views.xml",
+        "views/account_asset_category_views.xml",
+        "views/account_asset_views.xml",
+        "views/account_asset_impairment_common_views.xml",
+        "views/account_asset_impairment_views.xml",
+        "views/account_asset_impairment_reversal_views.xml",
     ]
 }
