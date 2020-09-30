49c4ba463fb6599f8f307b4e9961d7fe68503d56
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-30 14:12:17 +0700

    [8.0.1.0.0][fixed_asset_stock]

--- fixed_asset_stock/__openerp__.py
+++ fixed_asset_stock/__openerp__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Stock and Asset Management Integration",
-    "version": "8.0.2.0.0",
+    "version": "8.0.1.0.0",
     "category": "Accounting & Finance",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "account_asset_management_extend",
+        "fixed_asset",
         "stock_account",
     ],
     "data": [
