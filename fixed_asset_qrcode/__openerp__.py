26be1ee871c6209b5b3b04f73fef667707623965
mikevhe18 <viriyananda.michael@gmail.com>
2020-10-09 11:37:43 +0700

    [8.0.1.0.0][fixed_asset_qrcode]

--- fixed_asset_qrcode/__openerp__.py
+++ fixed_asset_qrcode/__openerp__.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fixed Asset QR Code",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.0.0",
     "category": "Accounting & Finance",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "account_asset_management",
+        "fixed_asset",
         "base_qr_code",
     ],
     "data": [
         "data/base_qr_content_policy_data.xml",
         "views/account_asset_views.xml",
-    ]
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
 }
--- fixed_asset_qrcode/models/account_asset.py
+++ fixed_asset_qrcode/models/account_asset.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openerp import models
 
 
 class AccountAssetAsset(models.Model):
     _name = "account.asset.asset"
-    _inherit = ["account.asset.asset", "base.qr_document"]
+    _inherit = [
+        "account.asset.asset",
+        "base.qr_document",
+    ]
