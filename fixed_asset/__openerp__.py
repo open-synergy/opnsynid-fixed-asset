674a8f675801b8ebbebeed31af1000bdc59e9f8f
Andhitia Rama <andhitia.r@gmail.com>
2020-10-27 11:35:05 +0700

    [fixed_asset] 8.0.2.0.1
    * Fix unlink depreciation line move

--- fixed_asset/__openerp__.py
+++ fixed_asset/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Fixed Asset",
-    "version": "8.0.2.0.0",
+    "version": "8.0.2.0.1",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
