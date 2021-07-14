dc563c6aefa9a6f2a0ee8574b609cf2c88212c64
Nurazmi <azmimr67@gmail.com>
2021-07-14 14:22:34 +0700

    [8.0.1.0.2] fixed_asset_estimation_change
    Fix #30

--- fixed_asset_estimation_change/__openerp__.py
+++ fixed_asset_estimation_change/__openerp__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fixed Asset Change of Estimation",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.0.2",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -31,5 +30,5 @@
         "views/account_asset_config_setting_views.xml",
         "views/account_asset_change_estimation_useful_life_views.xml",
         "views/account_asset_change_estimation_salvage_views.xml",
-    ]
+    ],
 }
