669094fceade8e99e1193eca4c852f34db0da00d
Nurazmi <azmimr67@gmail.com>
2021-07-14 13:05:40 +0700

    [8.0.1.0.1] fixed_asset_capital_improvement
    Fix #29

--- fixed_asset_capital_improvement/__openerp__.py
+++ fixed_asset_capital_improvement/__openerp__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fixed Asset Improvement",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -30,5 +29,5 @@
         "views/account_asset_config_setting_views.xml",
         "views/account_asset_category_views.xml",
         "views/account_asset_improvement_views.xml",
-    ]
+    ],
 }
