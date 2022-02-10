3a29273370e6ade18576b4c4cfaeae0fcdcc5840
mikevhe18 <viriyananda.michael@gmail.com>
2022-02-10 19:17:46 +0700

    [fixed_asset] 11.0.1.0.0

---
+++ fixed_asset/__manifest__.py
@@ -0,0 +1,45 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Fixed Asset",
+    "version": "11.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+        "account_fiscal_year",
+        "ssi_sequence_mixin",
+        "ssi_policy_mixin",
+        "ssi_multiple_approval_mixin",
+    ],
+    "external_dependencies": {
+        "python": [
+            "ddt",
+        ],
+    },
+    "data": [
+        "security/account_asset_security.xml",
+        "security/ir.model.access.csv",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "menu.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "views/fixed_asset_config_setting_views.xml",
+        "views/fixed_asset_category.xml",
+        "views/account_account_view.xml",
+        "views/account_move_line_view.xml",
+        "views/account_move_view.xml",
+        "views/fixed_asset_asset.xml",
+        "views/account_invoice_view.xml",
+    ],
+    "demo": [],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ fixed_asset/models/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import (
+    res_config,
+    fixed_asset_category,
+    account_account,
+    fixed_asset_asset,
+    fixed_asset_depreciation_line_subtype,
+    fixed_asset_depreciation_line,
+    account_move_line,
+    account_invoice_line,
+    account_invoice,
+)
