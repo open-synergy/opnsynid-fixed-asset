539b2c43eecef2320777c15b2d5c6452cfbe2d31
usa8bit <usa8bit@gmail.com>
2023-03-20 07:05:48 +0700

    [ssi_fixed_asset_salvage_value_estimation_change] 14.0.1.0.0

---
+++ ssi_fixed_asset_salvage_value_estimation_change/__manifest__.py
@@ -0,0 +1,36 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Fixed Asset Salvage Value Estimation Change",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "ssi_financial_accounting",
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "views/fixed_asset_salvage_value_estimation_change_views.xml",
+    ],
+    "demo": [],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
