c4c8d6d6133fae4ce9d166f21d7701d13b27038f
Miftahussalam <miftah_salam@ymail.com>
2023-03-25 09:44:25 +0700

    [ssi_fixed_asset_report] 14.0.1.0.0

---
+++ ssi_fixed_asset_report/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Fixed Asset Report",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "ssi_fixed_asset",
+    ],
+    "external_dependencies": {
+
+    },
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/wizard_fixed_yearly_asset.xml",
+        "reports.xml",
+        "report/templates/fixed_asset_yearly.xml",
+    ],
+    "demo": [
+
+    ],
+    "images": [
+
+    ],
+}
