df54d47ec7cd8f92f7995ea72791ee7db5695dfd
usa8bit <usa8bit@gmail.com>
2023-05-18 10:38:22 +0700

    [ssi_fixed_asset_qrcode] 14.0.1.0.0

---
+++ ssi_fixed_asset_qrcode/models/fixed_asset_asset.py
@@ -0,0 +1,12 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class FixedAssetAsset(models.Model):
+    _name = "fixed.asset.asset"
+    _inherit = ["fixed.asset.asset", "mixin.qr_code"]
+
+    _qr_code_create_page = True
