b42179a0783f4c98a97a768a1facfd577ccf9c5b
usa8bit <usa8bit@gmail.com>
2023-06-29 13:24:20 +0700

    [ssi_fixed_asset_complex_asset] 14.0.1.0.0

---
+++ ssi_fixed_asset_complex_asset/__manifest__.py
@@ -0,0 +1,36 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Complex Fixed Asset",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_financial_accounting",
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_fixed_asset",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "views/complex_fixed_asset_installation_views.xml",
+        "views/complex_fixed_asset_removal_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ ssi_fixed_asset_complex_asset/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import (
+    complex_fixed_asset_installation,
+    complex_fixed_asset_removal,
+)
