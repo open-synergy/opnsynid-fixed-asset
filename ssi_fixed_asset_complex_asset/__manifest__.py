61b0d56fadbf8b501b334bd9c346cc7f06c79c0f
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-16 17:54:05 +0700

    [ssi_fixed_asset_complex_asset] Change License To AGPL-3

--- ssi_fixed_asset_complex_asset/__manifest__.py
+++ ssi_fixed_asset_complex_asset/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Complex Fixed Asset",
     "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "application": True,
     "depends": [
