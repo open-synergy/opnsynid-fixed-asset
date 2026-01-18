e3bfc0ddaed695c871ca8a02498a4e36c41d15d6
Andhitia Rama <andhitia.r@gmail.com>
2026-01-18 11:51:46 +0700

    [UPD] ssi_fixed_asset_salvage_value_estimation_change
    * Memperbaiki F401

--- ssi_fixed_asset_salvage_value_estimation_change/__init__.py
+++ ssi_fixed_asset_salvage_value_estimation_change/__init__.py
@@ -2,6 +2,6 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import (
+from . import (  # noqa: F401
     models,
 )
--- ssi_fixed_asset_salvage_value_estimation_change/models/__init__.py
+++ ssi_fixed_asset_salvage_value_estimation_change/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import fixed_asset_salvage_value_estimation_change
+from . import fixed_asset_salvage_value_estimation_change  # noqa: F401
