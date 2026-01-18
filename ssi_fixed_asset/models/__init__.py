9221f1a73730948b602518ec3e56efcedbf5f91d
Andhitia Rama <andhitia.r@gmail.com>
2026-01-18 11:45:58 +0700

    [UPD] ssi_fixed_asset
    * Memperbaiki F401

--- ssi_fixed_asset/models/__init__.py
+++ ssi_fixed_asset/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import (  # noqa
+from . import (  # noqa: F401
     fixed_asset_category,
     account_account,
     fixed_asset_asset,
