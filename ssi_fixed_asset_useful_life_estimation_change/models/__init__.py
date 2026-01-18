ae75cf275cb754a98b91e7a3fb3e163152c0d230
Andhitia Rama <andhitia.r@gmail.com>
2026-01-18 11:52:08 +0700

    [UPD] ssi_fixed_asset_useful_life_estimation_change
    * Memperbaiki F401

--- ssi_fixed_asset_useful_life_estimation_change/models/__init__.py
+++ ssi_fixed_asset_useful_life_estimation_change/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import fixed_asset_useful_life_estimation_change
+from . import fixed_asset_useful_life_estimation_change  # noqa: F401
