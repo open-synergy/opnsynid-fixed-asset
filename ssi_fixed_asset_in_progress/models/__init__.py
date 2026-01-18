82d2ab3580b7f387dcc61f5528a3258be43a7d8c
Andhitia Rama <andhitia.r@gmail.com>
2026-01-18 11:47:42 +0700

    [UPD] ssi_fixed_asset_in_progress
    * Memperbaiki F401

--- ssi_fixed_asset_in_progress/models/__init__.py
+++ ssi_fixed_asset_in_progress/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import (
+from . import (  # noqa: F401
     fixed_asset_in_progress,
     account_move_line,
     fixed_asset_asset,
