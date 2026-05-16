1227b296624c8455a98b15911548d890d920f5f8
Andhitia Rama <andhitia.r@gmail.com>
2026-05-17 03:27:08 +0700

    [UPD] ssi_fixed_asset_disposal
    * Add unit tests

---
+++ ssi_fixed_asset_disposal/tests/test_fixed_asset_disposal.py
@@ -0,0 +1,13 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestFixedAssetDisposal(YamlTransactionCase):
+    def test_fixed_asset_disposal(self):
+        self.run_yaml_scenario("test_data_fixed_asset_disposal.yaml")
